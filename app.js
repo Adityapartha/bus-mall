f4ccc1cc13f12e89dc5b3f6d2dfcced632e47832
aditya parthasarathy <surayasun59@gmail.com>
2019-01-23 22:24:29 -0800

    day3lab

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-'use strict'
+'use strict';
 
 var allBusSmallPic =[];
 var bussmallpic = document.getElementById('bussmallpic');
@@ -7,16 +6,16 @@ var bussmallpic0 = document.getElementById('bussmallpic0');
 var bussmallpic1 = document.getElementById('bussmallpic1');
 var list = document.getElementById('options');
 var totalClick = 0;
-var i=0;
-// var myChart;
-
-createChart();
+var votes = [];
+var imagestags= [bussmallpic, bussmallpic0, bussmallpic1];
+var preRandomNumber =[];
 
 
 function Product(name) {
     this.filepath= `bussmallpic/${name}`;
     this.name= name;
     this.view = 0;
+    this.click = 0;
     allBusSmallPic.push(this);
 }
 
@@ -41,18 +40,22 @@ new Product('usb.gif');
 new Product('water-can.jpg');
 new Product('wine-glass.jpg');
 
+var render =JSON.parse( localStorage.getItem ('ProductStringified'));
 
-var votes = [];
-var title = [];
+if(render === null ){ 
+    showRandomPic();
+}else{
+    allBusSmallPic = render;
+    showRandomPic();
+}
 
 
-var imagestags= [bussmallpic, bussmallpic0, bussmallpic1];
 var preRandomNumber =[];
 
 function showRandomPic(){
     var randomNumbers =[];
 
-    for (i = 0; i < 3; i++) {
+    for (var i = 0; i < imagestags.length; i++) {
         var random = Math.floor(Math.random() * allBusSmallPic.length);
         imagestags[i].src = allBusSmallPic[random].filepath;
         imagestags[i].alt = allBusSmallPic[random].name;
@@ -69,119 +72,103 @@ function showRandomPic(){
         }
       }
       preRandomNumber = randomNumbers.slice(0);
-      console.log(preRandomNumber);
     }
-showRandomPic();
-
 
 
 list.addEventListener('click', handleClick);
 function handleClick(event){
-    console.log(event.target.alt);
     totalClick++;
-    console.log(totalClick);
-    for(var i; i < allBusSmallPic.length; i++){
-      if(event.target.alt = allBusSmallPic[i].name){
+    // console.log(totalClick);
+    for(var i = 0; i < allBusSmallPic.length; i++){
+      if(event.target.alt === allBusSmallPic[i].name){
           console.log('was clicked', event.target.alt);
           allBusSmallPic[i].click++
       }
     }
     if(totalClick === 25){
         list.removeEventListener('click', handleClick);
-        
-        return;
-    }
-    showRandomPic();
-
-}
-
-function handlesClick(event) {
-    for( var i = 0; i < 25; i++){
-        if(event.target.alt === allBusSmallPic[i].name){
-            allBusSmallPic[i].view++;
+        updateChartArray();
+    var ctx = document.getElementById('myChart')
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['bag','banana', 'bathroom','boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
+            datasets: [{
+                label: 'Total votes',
+                data: votes,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
-    }
+    });
+    localStorage.setItem ('ProductStringified', JSON.stringify(allBusSmallPic));
+    return;
 }
-// totalClick++;
-// if(totalClick === 25){
-//     option.removeEventListener('click', handleClick);
-//     console.log('hy');
-//     updateChartArray();
-    
-// }
-// showRandomPic();
 
-// createChart();
-
+    showRandomPic();
+}
 
 
 function updateChartArray(){
     
-    // console.log('hi');
     for (var i =0; i< allBusSmallPic.length; i++){
-        title[i]  = allBusSmallPic[i].name;
-        votes[i] = allBusSmallPic[i].view;
+      votes[i] = allBusSmallPic[i].click;
     }
 }
 
-function tallyVotes(thisPic) {
-       for(var i =0; i < allBusSmallPic.length; i++){
-           if(thisPic === allBusSmallPic[i].click){
-               allBusSmallPic[i].click++;
-               updateChartArray();
-   }
-       }
-   }
-//    var data = {
-//        labels: title,
-//        datasets:[{
-//            data: votes,
-//        }]
-//    }
 
-
-
-function createChart(){
-
-var ctx = document.getElementById('myChart');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['bag','banana', 'bathroom','boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'],
-        datasets: [{
-            label: 'Total votes',
-            data: votes,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-}
-
-    showRandomPic();
+  
 
 
